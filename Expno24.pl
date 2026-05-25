5eceaae29f1a25985a897552f39da2593681a494
vignesh1124 <vigneshganesh209@gmail.com>
2026-05-25 12:56:42 +0530

    Add files via upload

---
+++ Expno24.pl
@@ -0,0 +1,13 @@
+food(apple,   fruit,     sweet,  low_calorie).
+food(carrot,  vegetable, savory, low_calorie).
+food(chicken, meat,      savory, high_protein).
+food(fish,    seafood,   savory, high_protein).
+food(spinach, vegetable, savory, high_iron).
+food(almonds, nut,       savory, high_fat).
+
+diet(heart_disease, [apple, carrot, chicken, fish, almonds]).
+diet(diabetes,      [apple, carrot, fish, spinach, almonds]).
+diet(anemia,        [spinach, chicken, fish, almonds]).
+diet(obesity,       [apple, carrot, fish, spinach]).
+
+suggest_diet(Disease, Diet) :- diet(Disease, Diet).
