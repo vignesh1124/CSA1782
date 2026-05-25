12dcc6cddaabcef897610ccccde36b06a0ab4020
vignesh1124 <vigneshganesh209@gmail.com>
2026-05-25 12:50:10 +0530

    Add files via upload

---
+++ Expno18.pl
@@ -0,0 +1,9 @@
+% Facts
+dob(john, date(1990, 5, 1)).
+dob(jane, date(1985, 12, 10)).
+dob(bob, date(1978, 2, 28)).
+dob(sue, date(1995, 8, 15)).
+dob(tom, date(2000, 4, 22)).
+
+% Rule
+lookup(Name, DOB) :- dob(Name, DOB).
